2974d4ad7f5583e2a2183562e7d3e458d7cd62ad
ehlwas <myramkenacc@gmail.com>
2025-02-04 23:37:25 +0400

    m

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import Button from '../common/custom/Button'
+import { useTheme } from '../../hooks/ThemeProvider';
 
 const Header = () => {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  }
+  
   return (
     <div className='sticky top-0 z-50 bg-white/30 dark:bg-black/30'>
       <div className="flex justify-between items-center max-w-[1600px] mx-auto p-5 dark:text-white">
 
-        <a href="#">MEATBALLS</a>
+        <div onClick={toggleTheme}>MEATBALLS</div>
 
         <div className="flex items-center gap-5">
 
